a7fe82180fdda5367a3975853f89d023305fbe94
Fardeen khan <76776528+fdkofficial@users.noreply.github.com>
2021-05-01 17:52:09 +0530

    again

--- Fdkproject/settings.py
+++ Fdkproject/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
