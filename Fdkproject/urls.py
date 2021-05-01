6a14e843733c3b87cc31e4c9f5f326885680aa01
Fardeen khan <76776528+fdkofficial@users.noreply.github.com>
2021-05-01 17:40:42 +0530

    Update urls.py

--- Fdkproject/urls.py
+++ Fdkproject/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path('signup/',views.signup,name='handlesignup'),
     path('signin/',views.signin),
     path('signout/',views.logout1)
+     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
